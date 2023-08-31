e296fa3fc749c05dfa7ca63d431832a227dc70e2
chitkoo <chitkokooo196@gmail.com>
2023-08-31 21:43:40 +0630

    finished

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -10,88 +10,98 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(
-        title: const Text('To Do List'),
-        actions: [
-          IconButton(
-            onPressed: () => context.read<TodoBloc>().add(DeleteAllTodos()),
-            icon: const Icon(Icons.remove_circle),
-            color: Colors.red,
-            tooltip: 'Remove All',
-          ),
-        ],
-      ),
-      backgroundColor: Colors.grey.shade100,
-      body: BlocBuilder<TodoBloc, TodoState>(
-        builder: (context, state) {
-          if (state.loadingStatus == LoadingStatus.loading) {
-            return const Center(
-              child: CupertinoActivityIndicator(),
-            );
-          }
-
-          if (state.loadingStatus == LoadingStatus.failed) {
-            return const Center(
-              child: Text('Unale to load todos'),
-            );
-          }
-
-          if (state.loadingStatus == LoadingStatus.succeed &&
-              state.todos.isEmpty) {
-            return const Center(
-              child: Text('Add to do list'),
-            );
-          }
-
-          return ListView.builder(
-            itemCount: state.todos.length,
-            itemBuilder: (context, index) {
-              Todo todo = state.todos[index];
-
-              return Container(
-                width: double.infinity,
-                margin: const EdgeInsets.symmetric(
-                  horizontal: 20,
-                  vertical: 5,
-                ),
-                decoration: ShapeDecoration(
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                  color: Colors.white,
-                ),
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text('ID : ${todo.id} => '),
-                    Expanded(child: Text(todo.task)),
-                    IconButton(
-                        onPressed: () {
-                          _showModalBottomSheet(context: context, todo: todo);
-                        },
-                        icon: const Icon(Icons.edit)),
-                    IconButton(
-                      onPressed: () {
-                        context.read<TodoBloc>().add(DeleteTodo(todo: todo));
-                      },
-                      icon: const Icon(Icons.delete),
+    return BlocBuilder<TodoBloc, TodoState>(
+      builder: (context, state) {
+        return Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(
+            title: const Text('To Do List'),
+            actions: [
+              state.todos.length >= 2
+                  ? IconButton(
+                      onPressed: () =>
+                          context.read<TodoBloc>().add(DeleteAllTodos()),
+                      icon: const Icon(Icons.remove_circle),
                       color: Colors.red,
+                      tooltip: 'Remove All',
+                    )
+                  : const SizedBox.shrink(),
+            ],
+          ),
+          backgroundColor: Colors.grey.shade100,
+          body: BlocBuilder<TodoBloc, TodoState>(
+            builder: (context, state) {
+              if (state.loadingStatus == LoadingStatus.loading) {
+                return const Center(
+                  child: CupertinoActivityIndicator(),
+                );
+              }
+
+              if (state.loadingStatus == LoadingStatus.failed) {
+                return const Center(
+                  child: Text('Unale to load todos'),
+                );
+              }
+
+              if (state.loadingStatus == LoadingStatus.succeed &&
+                  state.todos.isEmpty) {
+                return const Center(
+                  child: Text('Add to do list'),
+                );
+              }
+
+              return ListView.builder(
+                itemCount: state.todos.length,
+                itemBuilder: (context, index) {
+                  Todo todo = state.todos[index];
+
+                  return Container(
+                    width: double.infinity,
+                    margin: const EdgeInsets.symmetric(
+                      horizontal: 20,
+                      vertical: 5,
                     ),
-                  ],
-                ),
+                    decoration: ShapeDecoration(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15),
+                      ),
+                      color: Colors.white,
+                    ),
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 15, vertical: 10),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text('ID : ${todo.id} => '),
+                        Expanded(child: Text(todo.task)),
+                        IconButton(
+                            onPressed: () {
+                              _showModalBottomSheet(
+                                  context: context, todo: todo);
+                            },
+                            icon: const Icon(Icons.edit)),
+                        IconButton(
+                          onPressed: () {
+                            context
+                                .read<TodoBloc>()
+                                .add(DeleteTodo(todo: todo));
+                          },
+                          icon: const Icon(Icons.delete),
+                          color: Colors.red,
+                        ),
+                      ],
+                    ),
+                  );
+                },
               );
             },
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _showModalBottomSheet(context: context),
-        child: const Icon(Icons.add),
-      ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () => _showModalBottomSheet(context: context),
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
     );
   }
 
@@ -147,7 +157,6 @@ class HomeScreen extends StatelessWidget {
                   context.read<TodoBloc>().add(AddTodo(todo: todo));
                 }
                 Navigator.pop(context);
-
               },
               child: const Text('Save'),
             ),
